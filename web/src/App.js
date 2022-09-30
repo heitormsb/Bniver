7eb10a192f73e365f2d65330a6ac69e9d1ccd7d4
heitormsb <heitormsb@gmail.com>
2022-09-30 10:40:16 -0300

    add: rotas enigma

--- web/src/App.js
+++ web/src/App.js
@@ -2,14 +2,19 @@ import React from "react";
 import "./App.css";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home/index";
+import EnigmaFuncao from "./pages/EnigmaFuncao";
+import EnigmaCoord from "./pages/EnigmaCoord";
+import EnigmaCruz from "./pages/EnigmaCruz";
 import Tome from "./pages/Tome";
 
 function App() {
   return (
     <Router>
       <Routes> 
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/tome" element={<Tome />} />
+        <Route exact path="/home" element={<Home />} />
+        <Route exact path="/forma" element={<EnigmaFuncao />} />
+        <Route exact path="/coord" element={<EnigmaCoord />} />
+        <Route exact path="/tau" element={<EnigmaCruz />} />
       </Routes>
     </Router>
   );
